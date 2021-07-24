8bdcdc13e70f72cf6e2e863bab33a662d2f8dd44
jokatty <jyotikattani@gmail.com>
2021-07-24 16:20:08 +0800

    add example component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+// import your components
+import ExampleComponent from './components/ExampleComponent.jsx';
 
 export default function App() {
   return (
     <div>
       This is App.jsx
+      {/* Add components here. Example:
+      <ExampleComponent />
+       */}
+      <ExampleComponent />
     </div>
   );
 }
