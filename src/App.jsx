965b0dc57c3d089e52df9092f61b5511e2a51baf
Kai Yuan Neo <kai@rocketacademy.co>
2022-04-11 22:41:43 +0800

    lint App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,33 @@ import React, { useState } from 'react';
 export default function App() {
   // array to use with map
   const [board, setBoard] = useState([
-    ['','',''],
-    ['','',''],
-    ['','',''],
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
   ]);
   const [currentPlayer, setCurrentPlayer] = useState('X');
 
   const handleClick = (rowIndex, colIndex) => {
-    let newBoard = [...board];
+    const newBoard = [...board];
     newBoard[rowIndex][colIndex] = currentPlayer;
     setBoard([...newBoard]);
     currentPlayer === 'X' ? setCurrentPlayer('O') : setCurrentPlayer('X');
-  }
+  };
 
   const rows = board.map((row, rowIndex) => {
     // row is one *unnested* array
     console.log('row', row);
     const buttons = row.map((square, colIndex) => (<button onClick={() => handleClick(rowIndex, colIndex)}>{square}</button>));
     return (
-    <div className="boardRow">
-      {buttons}
-    </div>);
+      <div className="boardRow">
+        {buttons}
+      </div>
+    );
   });
 
   return (
     <div className="board">
       {rows}
     </div>
-  )
-};
+  );
+}
